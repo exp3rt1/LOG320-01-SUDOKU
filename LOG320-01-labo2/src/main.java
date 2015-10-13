51923a1122f4849dac32ba1ea0fe72562f85261c
SamsonJordan <samso_000@10.196.122.25>
2015-10-13 12:24:04 -0400

    Commit le main for reasons

--- LOG320-01-labo2/src/main.java
+++ LOG320-01-labo2/src/main.java
@@ -1,3 +1,7 @@
+import java.util.Enumeration;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
 
 public class main {
 	static Table table = new Table();
@@ -13,10 +17,12 @@ public class main {
 	public void algorithme()
 	{
 		recursif1(0,0);
+		
+		// Si ca vient ici, le sudoku est invalid
 	}
 	
 	//TODO: a revoir, je vais regarder plus en détail mardi
-	public void recursif(Table table, int i, int j, Case tableCase)
+	/*public void recursif(Table table, int i, int j, Case tableCase)
 	{
 		i=0;
 		j=0;
@@ -42,7 +48,7 @@ public class main {
 		++j;
 		
 		recursif(table, i, j, tableCase);
-	}
+	}*/
 	
 	
 	//TODO: a revoir
@@ -50,26 +56,93 @@ public class main {
 	{
 		Case tableCase = table.getCase(ligne, colonne);
 		
-		// Arraylist?
-		for(int i=0; i < tableCase.hintArray.length; i++)
+		// Si la case a déjà une valeur, donc le nombre d'indice est à 0
+		if(tableCase.caseValue == 0)
 		{
-			// vérifie si il y a pas d'erreur causé par des case avant
-			if(!table.getBlock(ligne,colonne).validBlock())
-				return;
-			
-			// Met la valeur dans le tableau
-			tableCase.caseValue = tableCase.hintArray[i];
-			
-			// enlever les indices semblables à celui qui vient d'etre insérer sur la ligne, colonnes, et block
-		
 			if(colonne == 8)
 			{
-				recursif1(ligne++, 0);
+				if(ligne != 8)
+				{
+					colonne = 0;
+					tableCase = table.getCase(ligne++, colonne);
+				}
 			}
 			else
 			{
-				recursif1(ligne, colonne++);
+				tableCase = table.getCase(ligne, colonne++);
+
+			}
+		}
+		
+		Iterator<Map.Entry<Integer, Integer>> iterateur = tableCase.hintHashTable.entrySet().iterator();
+		
+		while(iterateur.hasNext())
+		{		
+			// Met la valeur dans le tableau
+			tableCase.caseValue = iterateur.next().getKey();
+			
+			// enlever les indices semblables à celui qui vient d'etre insérer sur la ligne, colonnes, et block
+			
+			// si la case mise est valide
+			if(!table.caseValide(tableCase.caseValue, ligne, colonne))
+			{
+				if(colonne == 8)
+				{
+					if(ligne != 8)
+					{
+						// derniere
+						recursif1(ligne+1, 0);
+					}
+				}
+				else
+				{
+					recursif1(ligne, colonne+1);
+				}
+			}
+			
+			
+		}
+	}
+	
+	public void cleanupHints(Table table){
+		Case currentCase;
+		int compareValue;
+		Block currentBlock;
+		for(int j = 0; j<9; j++){//col
+			for(int i = 0; i<9; i++){//lig
+				currentCase = table.getCase(i, j);
+				if (currentCase.caseValue == 0){ //if case is empty
+					
+				//check in the block
+					currentBlock = table.getBlock(i, j);
+					for(int c = 0; c<3; c++){
+						for(int l=0; l<3;l++){
+							compareValue = currentBlock.getCase(l, c).caseValue;
+							if( compareValue > 0){//if the block case has a value
+								//remove the key from the possible hints
+								currentCase.hintHashTable.remove(compareValue);
+							}
+						}
+					}
+					
+				//check the line
+					for(int c = 0; c<9; c++){
+						compareValue = table.getCase(i, c).caseValue;
+						if(compareValue > 0){
+							currentCase.hintHashTable.remove(compareValue);
+						}
+					}
+					
+				//check the column
+					for(int l = 0; l<9; l++){
+						compareValue = table.getCase(l, j).caseValue;
+						if(compareValue > 0){
+							currentCase.hintHashTable.remove(compareValue);
+						}
+					}
+				}
 			}
 		}
 	}
+	
 }
