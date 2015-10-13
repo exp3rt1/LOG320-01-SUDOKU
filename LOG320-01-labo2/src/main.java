2e12a2fa37f8169f86573aa9dd297e9aa8dd5213
olivier <oli_9410@hotmail.com>
2015-10-12 20:40:02 -0400

    Algorithme backtracking: j'ai fait ma propre version sans enlever l'autre, mais il faut parler de quelques trucs avant de continuer.

--- LOG320-01-labo2/src/main.java
+++ LOG320-01-labo2/src/main.java
@@ -10,16 +10,14 @@ public class main {
 	
 	
 	
-	public void algorithme(){
-	    for(int i=0; i != 9; ++i){
-	    	for(int j=0; j != 9; ++j){
-		    	
-		    }
-	    }
+	public void algorithme()
+	{
+		recursif1(0,0);
 	}
 	
 	//TODO: a revoir, je vais regarder plus en détail mardi
-	public void recursif(Table table, int i, int j, Case tableCase){
+	public void recursif(Table table, int i, int j, Case tableCase)
+	{
 		i=0;
 		j=0;
 		int k=0;
@@ -29,8 +27,10 @@ public class main {
 		
 		tableCase = table.getCase(i, j);
 		
-		while(k != tableCase.hintArray.length){
-			if(tableCase.hintArray[k] != null){
+		while(k != tableCase.hintArray.length)
+		{
+			if(tableCase.hintArray[k] != null)
+			{
 				tableCase.caseValue = tableCase.hintArray[k];
 				table.removeHints(i,j, tableCase.hintArray[k]);
 				break;
@@ -43,5 +43,33 @@ public class main {
 		
 		recursif(table, i, j, tableCase);
 	}
-
+	
+	
+	//TODO: a revoir
+	public void recursif1(int ligne, int colonne)
+	{
+		Case tableCase = table.getCase(ligne, colonne);
+		
+		// Arraylist?
+		for(int i=0; i < tableCase.hintArray.length; i++)
+		{
+			// vérifie si il y a pas d'erreur causé par des case avant
+			if(!table.getBlock(ligne,colonne).validBlock())
+				return;
+			
+			// Met la valeur dans le tableau
+			tableCase.caseValue = tableCase.hintArray[i];
+			
+			// enlever les indices semblables à celui qui vient d'etre insérer sur la ligne, colonnes, et block
+		
+			if(colonne == 8)
+			{
+				recursif1(ligne++, 0);
+			}
+			else
+			{
+				recursif1(ligne, colonne++);
+			}
+		}
+	}
 }
