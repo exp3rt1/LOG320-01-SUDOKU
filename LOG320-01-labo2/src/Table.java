da93a62daf8b7f7daf75bb1d280fce31405ad32d
SamsonJordan <samso_000@10.196.122.25>
2015-10-13 11:15:54 -0400

    Changements a Table

--- LOG320-01-labo2/src/Table.java
+++ LOG320-01-labo2/src/Table.java
@@ -36,5 +36,83 @@ public class Table {
     	Block block = new Block(blockLine,blockLine+2,blockColumn, blockColumn+2);
     	return block;
     }
+    
+    public boolean caseValide(int nombre, int ligne, int colonne)
+    {
+    	if(this.blocValide(nombre, ligne, colonne))
+    	{
+    		if(this.colonneValide(nombre, ligne, colonne))
+    		{
+	    		if(this.ligneValide(nombre, ligne, colonne))
+	    		{
+	    			return true;
+	    		}
+    		}
+    	}
+    	
+    	return false;
+    }
+    
+    // le nombre a vérifié avec sa position en ligne et en colonne
+    public boolean ligneValide(int nombre, int ligne, int colonne)
+    {
+    	boolean valide = true;
+    	
+    	for(int i=0; i < 9; i++)
+		{
+			if(caseArray[i][colonne].caseValue == nombre && nombre != 0)
+			{
+				if(i != ligne)
+				{
+					valide = false;
+				}
+			}
+		}
+    	
+    	return valide;
+    }
+    
+    // le nombre a vérifié avec sa position en ligne et en colonne
+    public boolean colonneValide(int nombre, int ligne, int colonne)
+    {
+    	boolean valide = true;
+    	
+    	for(int i=0; i < 9; i++)
+		{
+			if(caseArray[ligne][i].caseValue == nombre && nombre != 0)
+			{
+				if(i != colonne)
+				{
+					valide = false;
+				}
+			}
+		}
+    	
+    	return valide;
+    }
+    
+    // le nombre a vérifié avec sa position en ligne et en colonne
+    public boolean blocValide(int nombre, int ligne, int colonne)
+    {
+    	boolean valide = true;
+    	int blocLigne = (int) Math.floor(ligne/3)*3;
+		int blocColonne = (int) Math.floor(colonne/3)*3;
+		
+		for(int i=blocLigne; i < blocLigne+3; i++)
+		{
+			for(int j=blocColonne; j < blocColonne+3; j++)
+			{
+				if(caseArray[i][j].caseValue == nombre && nombre != 0)
+				{
+					if(i != ligne && j != colonne)
+					{
+						valide = false;
+					}
+				}
+			}
+		}
+		
+		return valide;
+    }
 
 }
