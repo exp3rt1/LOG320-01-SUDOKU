a7457d7f01b0b4c166cda37afffd77d5a3bd18d7
SamsonJordan <samso_000@10.196.122.25>
2015-10-14 21:38:28 -0400

    plus petits a plus plus grand

--- LOG320-01-labo2/src/Table.java
+++ LOG320-01-labo2/src/Table.java
@@ -16,8 +16,8 @@ public class Table {
     	for(int i=0; i != totalNbOfLines; ++i){
     		for(int j=0; j !=totalNbOfColumns; ++j){
     			if((i == line || j == column) 
-    			        && (line < block.getLineStart() || block.getLineEnd() > line)
-    			        && (column < block.getColumnStart() || block.getColumnEnd() > column))
+    			        && (line < block.getLineStart() || block.getLineEnd() < line)
+    			        && (column < block.getColumnStart() || block.getColumnEnd() < column))
     				this.getCase(i,j).disableHint(hint);
     		}
     	}
@@ -37,8 +37,8 @@ public class Table {
         for(int i=0; i != totalNbOfLines; ++i){
             for(int j=0; j !=totalNbOfColumns; ++j){
                 if((i == line || j == column) 
-                        && (line < block.getLineStart() || block.getLineEnd() > line)
-                        && (column < block.getColumnStart() || block.getColumnEnd() > column))
+                        && (line < block.getLineStart() || block.getLineEnd() < line)
+                        && (column < block.getColumnStart() || block.getColumnEnd() < column))
                     this.getCase(i,j).enableHint(hint);
             }
         }
